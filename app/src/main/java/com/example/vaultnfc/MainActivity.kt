4d703818e642d597fe4daaac11dce3236ca7243e
Codinab <alex.codinab@gmail.com>
2024-04-10 18:56:44 +0200

    Logout Preference applied

--- app/src/main/java/com/example/vaultnfc/MainActivity.kt
+++ app/src/main/java/com/example/vaultnfc/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.vaultnfc
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.example.vaultnfc.ui.AppNavigation
 import com.example.vaultnfc.ui.theme.VaultNFCTheme
 import com.example.vaultnfc.ui.viewmodel.LoginViewModel
+import com.example.vaultnfc.ui.viewmodel.SettingsViewModel
 import com.example.vaultnfc.ui.viewmodel.SignInViewModel
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.api.ApiException
 
 class MainActivity : ComponentActivity() {
     private lateinit var viewModelProvider: SignInViewModel
     private lateinit var loginViewModel: LoginViewModel
-
-    private var currentLogoutOption: String = "Closing app"
-
+    private lateinit var settingsViewModel: SettingsViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,29 +24,19 @@ class MainActivity : ComponentActivity() {
 
         viewModelProvider = ViewModelProvider(this)[SignInViewModel::class.java]
         loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        settingsViewModel = ViewModelProvider(this)[SettingsViewModel::class.java]
 
-
-
-        //Not implemented
-        googleSignInLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                    try {
-                        viewModelProvider.signInSuccess()
-                    } catch (e: ApiException) {
-                        // The ApiException status code indicates the detailed failure reason.
-                        // Handle sign-in failure
-                        viewModelProvider.signInFailure()
-                    }
-                }
-            }
+        settingsViewModel.logoutTimerOption.asLiveData().observe(this) { option ->
+            currentLogoutOption = option
+        }
 
         setContent {
             VaultNFCTheme {
                 AppNavigation(this.application)
             }
         }
+
+
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -58,18 +44,18 @@ class MainActivity : ComponentActivity() {
 
     }
 
+    private var currentLogoutOption = "Closing app"
+    override fun onStop() {
+        super.onStop()
+
+            if (currentLogoutOption == "Closing app") {
+                loginViewModel.logout(this)
+            }
+    }
+
 
     private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
 
 
-
-    // Inside your main activity or wherever appropriate
-    override fun onStop() {
-        super.onStop()
-
-        if (currentLogoutOption == "Closing app") {
-            loginViewModel.logout(this)
-        }
-    }
 
 }
